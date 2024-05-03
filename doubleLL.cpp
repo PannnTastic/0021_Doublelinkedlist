bb0d14dfbc1bb15b9c77435381267968d9b9b160
PannnTastic <pannntastic@gmail.com>
2024-05-03 13:17:46 +0700

    menegisi prosedur addnode

--- doubleLL.cpp
+++ doubleLL.cpp
@@ -17,3 +17,39 @@ void addnode(){
     cin >> nodebaru->noMhs;
     cout << "Masukkan Nama : ";
     cin >> nodebaru->name;
+    if (START == NULL || nodebaru->noMhs <= START -> noMhs){
+        if(START != NULL && nodebaru->noMhs == START -> noMhs){
+            cout << "NIM sudah ada " << endl;
+            return;
+        }
+        nodebaru ->next = START;
+        if (START != NULL){
+            START->prev = nodebaru;
+        }
+        nodebaru->prev = NULL;
+        START = nodebaru;
+    }
+    else{
+        node *current = START;
+        node *previous = NULL;
+
+        while ( current != NULL && current->noMhs < nodebaru->noMhs){
+            previous = current;
+            current = current->next;
+        }
+
+        nodebaru->next = current;
+        nodebaru->prev = previous;
+
+        if (current != NULL){
+            current->prev = nodebaru;
+        }
+
+        if ( previous != NULL){
+            previous->next = nodebaru;
+        }
+        else{
+            START = nodebaru;
+        }
+    }
+}
